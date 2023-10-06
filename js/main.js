68364602e14c269d9e35d6c42fca531845f7f0f6
Gianluca-V <107764977+Gianluca-V@users.noreply.github.com>
2023-10-06 20:01:47 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,83 +1,78 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    (fetchData = async () =>{
+document.addEventListener("DOMContentLoaded", () => {
+    (fetchData = async () => {
         await fetch("../data.json")
-        .then(response => response.json())
-        .then(res =>{
-            generateStudent(res);
-        })
-        .catch(err=>{
-            console.log(`An error ocurred ${err}`)
-        })
+            .then(response => response.json())
+            .then(res => {
+                generateStudent(res);
+            })
+            .catch(err => {
+                console.log(`An error ocurred ${err}`)
+            })
     })()
-    
-    const generateStudent = (data) =>{
+
+    const generateStudent = (data) => {
         const studentsContainer = document.querySelector(".students__container");
-    
+
         console.log(data)
         data.forEach(student => {
             const studentElement = document.createElement("div")
             studentElement.classList.add("students__student");
-            const studentName =  document.createElement("h3");
+            const studentName = document.createElement("h3");
             studentName.classList.add("students__name");
-            studentName.textContent = student.name;
-    
-            const studentAge =  document.createElement("div")
+            studentName.textContent = `${student.name}, ${student.age}`;
+
+            const studentAge = document.createElement("div")
             studentAge.classList.add("students__age");
             studentAge.textContent = student.age;
-    
-            const studentDescription =  document.createElement("div");
+
+            const studentDescription = document.createElement("div");
             studentDescription.classList.add("students__description");
             studentDescription.textContent = student.description;
-            
-            studentElement.appendChild(studentName);
-            studentElement.appendChild(studentAge);
-            studentElement.appendChild(studentDescription);
-    
-            //student projects
-            const studentprojects =  document.createElement("dialog");
-            studentprojects.classList.add("students__projects");
 
-            student.projects.forEach(project => {
-                const projectElement = document.createElement("div")
-                projectElement.classList.add("students__project");
-    
-                const projectImg = document.createElement("img")
-                projectImg.classList.add("project__img");
-                projectImg.src = project.img;
-                projectImg.alt = project.name;
-    
-                const projectName = document.createElement("h3")
-                projectName.classList.add("project__name");
-                projectName.textContent = project.name;
-    
-                const projectDesc = document.createElement("p")
-                projectDesc.classList.add("project__description");
-                projectDesc.textContent = project.desc;
-    
-                projectElement.appendChild(projectImg);
-                projectElement.appendChild(projectName);
-                projectElement.appendChild(projectDesc);
-    
-                studentprojects.appendChild(projectElement);
-            })
-    
-            studentElement.appendChild(studentprojects);
+            studentElement.appendChild(studentName);
+            studentElement.appendChild(studentDescription);
             studentsContainer.appendChild(studentElement);
 
-            studentElement.addEventListener("click",()=>{
-                studentprojects.showModal()
+            studentElement.addEventListener("click", () => {
+                updateDialog(student);
             });
-
-            const closeElement = document.createElement("div");
-            closeElement.classList.add("close");
-            closeElement.addEventListener("click",()=>{
-                studentprojects.close();
-            })
-
-            closeElement.textContent = "X";
-            studentprojects.appendChild(closeElement);
         });
     }
-    
+
 })
 
+const updateDialog = (student) => {
+    const dialog = document.querySelector(".students__projects");
+    dialog.innerHTML = "";
+    dialog.showModal();
+    student.projects.forEach(project => {
+        const projectElement = document.createElement("div")
+        projectElement.classList.add("students__project");
+
+        const projectImg = document.createElement("img")
+        projectImg.classList.add("project__img");
+        projectImg.src = project.img;
+        projectImg.alt = project.name;
+
+        const projectName = document.createElement("h3")
+        projectName.classList.add("project__name");
+        projectName.textContent = project.name;
+
+        const projectDesc = document.createElement("p")
+        projectDesc.classList.add("project__description");
+        projectDesc.textContent = project.desc;
+
+        projectElement.appendChild(projectName);
+        projectElement.appendChild(projectDesc);
+        projectElement.appendChild(projectImg);
+        dialog.appendChild(projectElement);
+    });
+    const closeElement = document.createElement("div");
+    closeElement.classList.add("project__close");
+    closeElement.addEventListener("click", () => {
+        dialog.close();
+    })
+
+    closeElement.textContent = "X";
+    dialog.appendChild(closeElement);
+}
